6ee6495716c7a293dbc653a81f27f7ba8897fe5e
Richard L <richard.latham@yahoo.co.uk>
2025-04-12 13:32:13 +0100

    Change main to reflect code from react-board-games

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,57 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
+import styled from "styled-components";
+import App from './app/app.tsx';
+
+
+const ErrorFallbackStyled=styled.div`
+  font-size: large;
+  * {
+    margin-bottom: 0.5em;
+  }
+`;
+
+const ErrorMessage=styled.div`
+
+  span:first-child {
+    font-weight: bold;
+    color: red;
+    margin-right: 0.5em;
+  }
+`;
+
+const ErrorStack=styled.div`
+  font-size: small;
+  margin-left: 1em;
+`;
+
+// eslint-disable-next-line react-refresh/only-export-components
+function ErrorFallback({ error }: { error: Error }) {
+    return (
+        <ErrorFallbackStyled>
+            <ErrorMessage>
+                <span>Internal error:</span>
+                <span>{error.message}</span>
+            </ErrorMessage>
+
+            {error.stack &&
+        <div>
+            <div>Gory details:</div>
+            <ErrorStack>{error.stack}</ErrorStack> 
+        </div>
+            }
+     
+        </ErrorFallbackStyled>
+    );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+    >
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
